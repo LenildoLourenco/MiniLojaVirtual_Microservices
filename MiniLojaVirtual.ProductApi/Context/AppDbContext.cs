5ab84d211dc4b2963d62dcd2e81a6c6532f4649b
Lenildo <lourencolenildo@gmail.com>
2022-09-05 19:36:37 -0300

    Adicionar arquivos de projeto.

---
+++ MiniLojaVirtual.ProductApi/Context/AppDbContext.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MiniLojaVirtual.ProductApi.Models;
+
+namespace MiniLojaVirtual.ProductApi.Context;
+
+public class AppDbContext : DbContext
+{
+    public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
+    public DbSet<Category> Categories { get; set; }
+    public DbSet<Product> Products { get; set; }
+}
